596bb03f0cb03d4ce829d224b173589c658fee71
Christian Kakesa <christian.kakesa@gmail.com>
2016-01-02 02:27:19 +0100

    Add all GO build supported architecture.

--- release.sh
+++ release.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 PROGNAME=gosoul
-PLATFORMS="darwin/386 darwin/amd64 freebsd/386 freebsd/amd64 freebsd/arm linux/386 linux/amd64 linux/arm windows/386 windows/amd64"
+PLATFORMS="darwin/386 darwin/amd64 darwin/arm darwin/arm64 \
+dragonfly/amd64 \
+freebsd/386 freebsd/amd64 freebsd/arm \
+linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le \
+netbsd/386 netbsd/amd64 netbsd/arm \
+openbsd/386 openbsd/amd64 openbsd/arm \
+plan9/386 plan9/amd64 \
+solaris/amd64 \
+windows/386 windows/amd64"
+
 VERSION=$(git tag -l | sort | tail -n1)
 
 for PLATFORM in $PLATFORMS; do
@@ -11,5 +20,5 @@ for PLATFORM in $PLATFORMS; do
     ARCHIVE=$PROGNAME-$VERSION-$OS-$ARCH.tar.gz
     tar -czf $ARCHIVE $PROGNAME
     echo $ARCHIVE
-    sleep 1
+    sleep 2
 done
