d225d26ce6b97a490a9c59147e9d13beedbe84fc
Christian Kakesa <christian.kakesa@gmail.com>
2016-01-02 03:57:38 +0100

    Fix release.sh: remove platforms who gives error when compiling0.

--- release.sh
+++ release.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 PROGNAME=gosoul
-PLATFORMS="darwin/386 darwin/amd64 darwin/arm darwin/arm64 \
+PLATFORMS="darwin/386 darwin/amd64 \
 dragonfly/amd64 \
 freebsd/386 freebsd/amd64 freebsd/arm \
 linux/386 linux/amd64 linux/arm linux/arm64 linux/ppc64 linux/ppc64le \
 netbsd/386 netbsd/amd64 netbsd/arm \
 openbsd/386 openbsd/amd64 openbsd/arm \
-plan9/386 plan9/amd64 \
 solaris/amd64 \
 windows/386 windows/amd64"
 
@@ -16,9 +15,8 @@ VERSION=$(git tag -l | sort | tail -n1)
 for PLATFORM in $PLATFORMS; do
     OS=${PLATFORM%/*}
     ARCH=${PLATFORM#*/}
-    GOOS=$OS CGO_ENABLED=0 GOARCH=$ARCH go build -o $PROGNAME
+    GOOS=$OS CGO_ENABLED=0 GOARCH=$ARCH go build -ldflags "-X main.Version=$VERSION" -o $PROGNAME
     ARCHIVE=$PROGNAME-$VERSION-$OS-$ARCH.tar.gz
     tar -czf $ARCHIVE $PROGNAME
     echo $ARCHIVE
-    sleep 2
 done
